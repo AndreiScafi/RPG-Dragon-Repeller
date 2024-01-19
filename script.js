be457980a456dac991e7673fcdf946255376f797
Andrei Scafi <andreiscafi@hotmail.com>
2024-01-19 08:02:02 -0300

    Adding Javascript code part 11

--- script.js
+++ script.js
@@ -250,7 +250,17 @@ function easterEgg() {
 }
 
 function pick(guess) {
+    const numbers = [];
 
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11));
+    }
+
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+
+    for (let x = 1; x < 5; x++) {
+
+    }
 }
 
 function pickTwo() {
